199649ba19601c097361f6ee6f347c04ecdb1628
Andy Kwong <yantkwong@gmail.com>
2021-01-10 13:56:36 -0800

    removed comments

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-
-# https://api.ebay.com/buy/browse/v1/item_summary/search?q=drone&limit=3
-# api doc: https://developer.ebay.com/api-docs/buy/browse/resources/item_summary/methods/search#h2-samples
 import scraper
 import parser
 import user_io
